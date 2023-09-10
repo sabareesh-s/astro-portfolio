d73fcc8e15e884ebfedf62f6372cd9329e4fa99e
sabareesh-s <sabareesh1221@gmail.com>
2023-09-10 12:45:49 +0530

    added data and customization

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,27 +1,46 @@
 export type Project = {
   title: string;
   techs: string[];
-  link: string;
+  link?: string;
   isComingSoon?: boolean;
+  github?: string;
 };
 
 const projects: Project[] = [
   {
-    title: "Dictionary App",
-    techs: ["ReactJS (NextJS)", "react-query", "zod"],
-    link: "https://github.com/MaeWolff/dictionary-app",
+    title: "Tickete payment page UI",
+    techs: ["ReactJS (NextJS)", "TypeScript", "TailwindCSS"],
+    link: "https://unrivaled-beijinho-c3171e.netlify.app/",
+    github: "https://github.com/sabareesh-s/tickete-payment-page"
   },
   {
-    title: "Portfolio / Lina BLIDI",
-    techs: ["ReactJS (NextJS)", "TypeScript"],
-    link: "https://www.linablidi.fr/",
+    title: "Todo app with authentication",
+    techs: ["ReactJS", "TypeScript", "TailwindCSS", "Firebase"],
+    link: "https://graceful-lebkuchen-e3c284.netlify.app/",
+    github: "https://github.com/sabareesh-s/todolist",
   },
   {
-    title: "Portfolio / Template",
-    techs: ["Astro"],
-    link: "/",
-    isComingSoon: true,
+    title: "Weather app",
+    techs: ["ReactJS", "Styled components 💅"],
+    link: "https://reactjs-weather-search-app.netlify.app/",
+    github: "https://github.com/sabareesh-s/todolist",
   },
+  {
+    title: "3D landing page",
+    techs: ["ReactJS", "Three.js", "React Three Fiber (R3F)"],
+    github: "https://github.com/sabareesh-s/threejs-webpack-starter/commits/master",
+  },
+  {
+    title: "Book search with OpenLibrary API",
+    techs: ["HTML", "CSS", "API Integration"],
+    github: "https://github.com/sabareesh-s/author-search",
+  },
+  // {
+  //   title: "Portfolio / Template",
+  //   techs: ["Astro"],
+  //   link: "/",
+  //   isComingSoon: true,
+  // },
 ];
 
 export default projects;
